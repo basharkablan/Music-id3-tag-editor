3e4d4a1c0ea6e69a0ac95352b75d51c5e784a7fa
Bashar Kablan <34687794+basharkablan@users.noreply.github.com>
2019-05-21 17:02:01 +0300

    Add files via upload

---
+++ MusicID3Filler/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace MusicID3Filler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.artistTextBox = new System.Windows.Forms.TextBox();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.previousButton = new System.Windows.Forms.Button();
+            this.autoFillButton = new System.Windows.Forms.Button();
+            this.counterLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "Choose MP3 Files ...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(137, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(87, 42);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.ReadOnly = true;
+            this.nameTextBox.Size = new System.Drawing.Size(347, 20);
+            this.nameTextBox.TabIndex = 1;
+            this.nameTextBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Song Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Title :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Artist :";
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(87, 86);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(347, 20);
+            this.titleTextBox.TabIndex = 5;
+            // 
+            // artistTextBox
+            // 
+            this.artistTextBox.Location = new System.Drawing.Point(87, 130);
+            this.artistTextBox.Name = "artistTextBox";
+            this.artistTextBox.Size = new System.Drawing.Size(347, 20);
+            this.artistTextBox.TabIndex = 5;
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(295, 179);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(79, 23);
+            this.nextButton.TabIndex = 6;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // previousButton
+            // 
+            this.previousButton.Location = new System.Drawing.Point(72, 179);
+            this.previousButton.Name = "previousButton";
+            this.previousButton.Size = new System.Drawing.Size(79, 23);
+            this.previousButton.TabIndex = 6;
+            this.previousButton.Text = "Previous";
+            this.previousButton.UseVisualStyleBackColor = true;
+            this.previousButton.Click += new System.EventHandler(this.previousButton_Click);
+            // 
+            // autoFillButton
+            // 
+            this.autoFillButton.Location = new System.Drawing.Point(235, 11);
+            this.autoFillButton.Name = "autoFillButton";
+            this.autoFillButton.Size = new System.Drawing.Size(75, 23);
+            this.autoFillButton.TabIndex = 7;
+            this.autoFillButton.Text = "Auto Fill";
+            this.autoFillButton.UseVisualStyleBackColor = true;
+            this.autoFillButton.Click += new System.EventHandler(this.autoFillButton_Click);
+            // 
+            // counterLabel
+            // 
+            this.counterLabel.AutoSize = true;
+            this.counterLabel.Location = new System.Drawing.Point(211, 184);
+            this.counterLabel.Name = "counterLabel";
+            this.counterLabel.Size = new System.Drawing.Size(24, 13);
+            this.counterLabel.TabIndex = 8;
+            this.counterLabel.Text = "0/0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 262);
+            this.Controls.Add(this.counterLabel);
+            this.Controls.Add(this.autoFillButton);
+            this.Controls.Add(this.previousButton);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.artistTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox artistTextBox;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button previousButton;
+        private System.Windows.Forms.Button autoFillButton;
+        private System.Windows.Forms.Label counterLabel;
+
+    }
+}
+
---
+++ MusicID3Filler/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MusicID3Filler
+{
+    public partial class Form1 : Form
+    {
+        private TagLib.File f1;
+        private int current = 0;
+
+        private string[] paths;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                paths = openFileDialog1.FileNames;
+                UpdateCounter();
+                LoadCurrent();
+            }
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            saveCurrent();
+
+            if (current < paths.Length - 1)
+            {
+                current++;
+                UpdateCounter();
+                LoadCurrent();
+            }
+        }
+
+        private void previousButton_Click(object sender, EventArgs e)
+        {
+            saveCurrent();
+
+            if (current > 0)
+            {
+                current--;
+                UpdateCounter();
+                LoadCurrent();
+            }
+        }
+
+        private void autoFillButton_Click(object sender, EventArgs e)
+        {
+            string path = "";
+            string[] splits = null;
+            string[] splitter = new string[] { " - " };
+
+            for (int i = 0; i < paths.Length; i++)
+            {
+                f1 = TagLib.File.Create(paths[i]);
+
+                path = paths[i].Substring(paths[i].LastIndexOf('\\') + 1);
+                path = path.Replace(".mp3", "");
+
+                splits = path.Split(splitter, StringSplitOptions.None);
+                if (splits.Length == 1)
+                    f1.Tag.Title = splits[0];
+                else
+                {
+                    f1.Tag.Title = splits[1];
+                    f1.Tag.Artists = null;
+                    f1.Tag.Artists = new string[] { splits[0] };
+                }
+
+                f1.Save();
+            }
+        }
+
+        private void LoadCurrent()
+        {
+            f1 = TagLib.File.Create(paths[current]);
+            nameTextBox.Text = paths[current].Substring(paths[current].LastIndexOf('\\') + 1);
+            titleTextBox.Text = f1.Tag.Title;
+
+            if (f1.Tag.Artists.Length > 0)
+                artistTextBox.Text = f1.Tag.Artists[0];
+            else
+                artistTextBox.Text = "";
+        }
+
+        private void saveCurrent()
+        {
+            f1.Tag.Title = titleTextBox.Text;
+            if (artistTextBox.Text != "")
+            {
+                f1.Tag.Artists = null;
+                f1.Tag.Artists = new string[] {artistTextBox.Text};
+            }
+            f1.Save();
+        }
+
+        private void UpdateCounter()
+        {
+            counterLabel.Text = (current + 1) + "/" + paths.Length;
+        }
+    }
+}
